9ad1df6080fa399f9e9ac65391c166784045fa51
wojcikpawel <wojcikpawel25@gmail.com>
2021-02-10 08:46:59 +0100

    Updates

---
+++ precache-manifest.9811750e3792fcfedfedaa1fb83001f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c31676d1b9d2be5fbb6a1a5576a8c97",
+    "url": "/MemeGeneratorRepository/index.html"
+  },
+  {
+    "revision": "6c8d173ff985d14052c7",
+    "url": "/MemeGeneratorRepository/static/css/main.46f6fd83.chunk.css"
+  },
+  {
+    "revision": "9d721c6c7201d9cd8e35",
+    "url": "/MemeGeneratorRepository/static/js/2.cd5b6cf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MemeGeneratorRepository/static/js/2.cd5b6cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8d173ff985d14052c7",
+    "url": "/MemeGeneratorRepository/static/js/main.c0fa3ea0.chunk.js"
+  },
+  {
+    "revision": "a75a4af029758625605e",
+    "url": "/MemeGeneratorRepository/static/js/runtime-main.e744025c.js"
+  }
+]);
